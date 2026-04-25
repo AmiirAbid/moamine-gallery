52470c217e6aa2340feb1459bd209a91286cc6b8
AmiirAbid <amirabid401@gmail.com>
2026-04-25 20:27:43 +0100

    added shop page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/moafavicon.ico';
+	import favicon from '$lib/assets/favicon.ico';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import ThemeToggle from '$lib/components/ThemeToggle.svelte';
 	let { children } = $props();
---
+++ src/routes/shop/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { Phone } from '@lucide/svelte';
+</script>
+
+<div class="flex h-screen items-center justify-center text-center text-foreground flex-col gap-6">
+	<h2 class="text-5xl font-semibold">Coming Soon</h2>
+	<div class="flex flex-col items-center gap-4">
+		<h3 class="text-2xl font-light">Currently available for custom projects</h3>
+		<a href="tel:+21697494493" class="bg-foreground text-background px-4 py-2 rounded-full text-xl font-normal flex gap-2 items-center">
+			<Phone class="w-5 h-5 cursor-pointer"/>
+			Reach Out
+		</a>
+	</div>
+</div>
